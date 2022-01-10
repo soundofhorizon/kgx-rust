d149189b9b283faa1588be4a3bb64df5edcf6785
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2022-01-10 14:47:02 +0900

    [ADD] selectコマンドを追加

--- src/commands/admin_only.rs
+++ src/commands/admin_only.rs
@@ -6,6 +6,11 @@ use serenity::framework::standard::{
     CommandResult,
 };
 use crate::utils::*;
+use crate::schema::{
+    demo_auction_info::dsl::demo_auction_info as demo_auction_info_table,
+    channel_auction::dsl::channel_auction as channel_auction_table,
+};
+use crate::models::*;
 use diesel;
 use diesel::prelude::*;
 
@@ -23,7 +28,28 @@ async fn execute_sql(ctx: &Context, msg: &Message, args: Args) -> CommandResult 
 }
 
 
+#[command]
+async fn select(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let d = ctx.data.read().await;
+    let conn = d.get::<ConnectionMapKey>().unwrap().lock().await;
+
+    let result = match &args.single::<String>()?[..] {
+        "demo_auction_info" => {
+            let result: Vec<AuctionInfo> = demo_auction_info_table.load(&*conn)?;
+            result.iter().map(|row| format!("{:?}", row)).collect::<Vec<_>>().join("\n")
+        },
+        "channel_auction" => {
+            let result: Vec<ChannelAuction> = channel_auction_table.load(&*conn)?;
+            result.iter().map(|row| format!("{:?}", row)).collect::<Vec<_>>().join("\n")
+        },
+        _ => "設定されていないテーブルです".to_string(),
+    };
+    msg.channel_id.say(&ctx.http, result).await?;
+    Ok(())
+}
+
+
 #[group]
-#[commands(execute_sql)]
+#[commands(execute_sql, select)]
 #[required_permissions(ADMINISTRATOR)]
 pub struct AdminOnly;
