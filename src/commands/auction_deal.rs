5dac4fc70cd7c69779b26c5675d18ab3fd31e84e
kaeru82433413 <56305929+kaeru82433413@users.noreply.github.com>
2022-01-12 19:26:09 +0900

    [Refactor] auction_managerを追加

--- src/commands/auction_deal.rs
+++ src/commands/auction_deal.rs
@@ -7,7 +7,7 @@ use serenity::framework::standard::{
     CommandResult,
 };
 use crate::schema::{
-    demo_auction_info::dsl::{demo_auction_info, id as auction_id_col, last_tend as last_tend_col},
+    demo_auction_info::dsl::demo_auction_info,
     channel_auction::dsl::{channel_auction, channel as channel_col, auction as auction_col},
 };
 use crate::utils::*;
@@ -26,7 +26,7 @@ async fn start(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let result = channel_auction.filter(channel_col.eq(channel_id)).get_results::<ChannelAuction>(&conn)?;
     if let [ChannelAuction { auction, .. }] = result[..] {
         if let Some(auction_id) = auction {
-            msg.reply(&ctx.http, format!("既にオークションが開催されています (id:{})", auction_id)).await?;
+            msg.channel_id.say(&ctx.http, format!("既にオークションが開催されています (id:{})", auction_id)).await?;
             return Ok(());
         }
     } else {
@@ -75,54 +75,51 @@ async fn start(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 async fn tend(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult{
     let conn = ctx.get_connection().await;
 
-    let channel_id = msg.channel_id.0 as i64;
+    let manager = AuctionManager::from_channel(&conn, msg.channel_id)?;
 
-    let result = channel_auction.filter(channel_col.eq(channel_id)).get_results::<ChannelAuction>(&conn)?;
-    let auction_id = if let [ChannelAuction { auction, .. }] = result[..] {
-        match auction {
-            Some(auction_id) => auction_id,
-            None => {
-                msg.reply(&ctx.http, format!("オークションが開催されていません")).await?;
-                return Ok(());
-            },
+    let mut manager = match manager {
+        Ok(manager) => manager,
+        Err(error) => {
+            match error {
+                GetAuctionError::NotAuctionChannel => {
+                    msg.channel_id.say(&ctx.http, "このチャンネルはオークションチャンネルではありません").await?;
+                },
+                GetAuctionError::NotHeld => {
+                    msg.channel_id.say(&ctx.http, format!("オークションが開催されていません")).await?;
+                },
+                _ => unreachable!(),
+            }
+            return Ok(());
         }
-    } else {
-        msg.channel_id.say(&ctx.http, "このチャンネルはオークションチャンネルではありません").await?;
-        return Ok(());
     };
 
     let price: i32 = args.single()?;
-    let auction_info: AuctionInfo = demo_auction_info.filter(auction_id_col.eq(auction_id)).get_result(&conn)?;
 
-    if price <= auction_info.last_tend {
-        let content = if auction_info.last_tend == auction_info.start_price-1 {
-            // 入札なしのとき
-            format!("入札価格が開始価格({})より低いです", auction_info.start_price)
-        } else {
-            format!("入札価格が現在の入札価格({})以下です", auction_info.last_tend)
-        };
-        msg.channel_id.say(&ctx.http, content).await?;
-        return Ok(());
-    }
-
-    let author_name = &msg.author.name;
-    if let Some(bin_price) = auction_info.bin_price {
-        if price >= bin_price {
-            msg.channel_id.send_message(
-                &ctx, |m| {
-                    m.embed(|e| {
-                        e.description(format!("即決価格以上の入札がされました\n落札者: {}\n落札額: {}", author_name, price))
-                    })
-                }
-            ).await?;
-            diesel::update(channel_auction.find(channel_id)).set(auction_col.eq(None::<i32>)).execute(&conn)?;
-            return Ok(());
+    let tend_result = manager.tend(&conn, msg.author.id.0, price);
+    match tend_result {
+        Ok(finished) => {
+            if finished {
+                msg.channel_id.send_message(
+                    &ctx, |m| {
+                        m.embed(|e| {
+                            e.description(format!("即決価格以上の入札がされました\n落札者: {}\n落札額: {}", msg.author.name, price))
+                        })
+                    }
+                ).await?;
+            } else {
+                msg.channel_id.say(&ctx.http, "入札しました").await?;
+            }
+        },
+        Err(error) => {
+            let content = match error {
+                TendError::LessThanStartPrice => format!("入札価格が開始価格({})より低いです", manager.start_price),
+                TendError::LastTendOrLess => format!("入札価格が現在の入札価格({})以下です", manager.tend.last().unwrap().price),
+                TendError::SameTender => "同一人物による入札は出来ません。".into(),
+            };
+            msg.channel_id.say(&ctx.http, content).await?;
         }
     }
-
-    msg.channel_id.say(&ctx.http, "入札しました").await?;
-    diesel::update(demo_auction_info.find(auction_id)).set(last_tend_col.eq(price)).execute(&conn)?;
-
+    
     Ok(())
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -38,3 +38,115 @@ pub async fn insert_pool(client: &Client) {
     let mut data = client.data.write().await;
     data.insert::<PoolKey>(pool);
 }
+
+
+pub mod auction_manager {
+    use chrono::NaiveDateTime;
+    use serenity::model::id::ChannelId;
+    use diesel::prelude::*;
+    use crate::models::*;
+    use crate::schema::{
+        channel_auction::dsl::{channel_auction, channel as channel_col, auction as auction_col},
+        demo_auction_info::dsl::{demo_auction_info, id as auction_id_col, last_tend as last_tend_col},
+    };
+    use crate::utils::PooledPgConnection;
+
+    pub struct TendInfo {
+        pub tender_id: u64,
+        pub price: i32,
+    }
+
+    pub enum GetAuctionError {
+        NotAuctionChannel,
+        NotHeld,
+        InvalidId,
+    }
+
+    pub enum TendError {
+        LessThanStartPrice,
+        LastTendOrLess,
+        SameTender,
+    }
+    
+    pub struct AuctionManager {
+        pub channel_id: u64,
+        pub id: i32,
+        pub item: String,
+        pub tend: Vec<TendInfo>,
+        pub end_time: NaiveDateTime,
+        pub start_price: i32,
+        pub bin_price: Option<i32>,
+    }
+
+    impl AuctionManager {
+        pub fn from_channel(conn: &PooledPgConnection, ChannelId(id): ChannelId) -> QueryResult<Result<Self, GetAuctionError>> {
+            let result = channel_auction.filter(channel_col.eq(id as i64)).get_result::<ChannelAuction>(conn).optional()?;
+            if let Some(ChannelAuction { auction: auction_id, .. }) = result {
+                if let Some(auction_id) = auction_id {
+                    Ok(Self::from_id(conn, auction_id)?)
+                } else {
+                    Ok(Err(GetAuctionError::NotHeld))
+                }
+            } else {
+                Ok(Err(GetAuctionError::NotHeld))
+            }
+        }
+
+        pub fn from_id(conn: &PooledPgConnection, auction_id: i32) -> QueryResult<Result<Self, GetAuctionError>> {
+            let auction_info = demo_auction_info.filter(auction_id_col.eq(auction_id)).get_result::<AuctionInfo>(conn).optional()?;
+            if let Some(info) = auction_info {
+                let mut tend = vec![];
+                if info.last_tend >= info.start_price {
+                    tend.push(TendInfo { tender_id: 0, price: info.last_tend});
+                }
+                Ok(Ok(Self {
+                    channel_id: 0,
+                    id: info.id,
+                    item: info.item,
+                    tend,
+                    end_time: info.end_time,
+                    start_price: info.start_price,
+                    bin_price: info.bin_price,
+                }))
+            } else {
+                Ok(Err(GetAuctionError::InvalidId))
+            }
+        }
+
+        pub fn tend(&mut self, conn: &PooledPgConnection, tender_id: u64, tend_price: i32) -> Result<bool, TendError> {
+
+            let mut finish = false;
+            if let Some(bin_price) = self.bin_price {
+                if tend_price >= bin_price {
+                    finish = true;
+                }
+            }
+            
+            if let [.., TendInfo { tender_id: last_tender_id, price: last_tend_price }] = self.tend[..] {
+                if tender_id == last_tender_id && !finish {
+                    return Err(TendError::SameTender);
+                } else if tend_price <= last_tend_price {
+                    return Err(TendError::LastTendOrLess);
+                }
+            } else {
+                if tend_price < self.start_price {
+                    return Err(TendError::LessThanStartPrice);
+                }
+            }
+            let new_tend = TendInfo { tender_id, price: tend_price };
+            self.tend.push(new_tend);
+            diesel::update(demo_auction_info.find(self.id)).set(last_tend_col.eq(tend_price)).execute(conn).unwrap();
+            
+            if finish {
+                self.finish(conn);
+            }
+            
+            Ok(finish)
+        }
+        
+        pub fn finish(&self, conn: &PooledPgConnection) {
+            diesel::update(channel_auction).filter(auction_col.eq(Some(self.id))).set(auction_col.eq(None::<i32>)).execute(conn).unwrap();
+        }
+    }
+}
+pub use auction_manager::{AuctionManager, GetAuctionError, TendError};
